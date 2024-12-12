ee0d9e25b7fa214efa003bec09ea252e7c1d9fdd
narwa <narwandaru.nara@gmail.com>
2024-12-12 22:59:55 +0700

    feat: gh pages svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
-import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter()
+        adapter: adapter(),
+        paths: {
+            base: '/quiz-gpt',
+        },
+        prerender: {
+            default: true
+        }
     },
-    preprocess: vitePreprocess()
+    preprocess: vitePreprocess(),
 };
+
 export default config;
